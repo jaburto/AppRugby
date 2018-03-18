08beca1418d2cab0d7b6dc17a9c284bbbd2044fa
Jonatan E. Aburto <jonatan.app@gmail.com>
2018-03-18 01:20:10 -0500

    form
    form

--- resources/views/vendor/crudbooster/post_index_tablaposicion.blade.php
+++ resources/views/vendor/crudbooster/post_index_tablaposicion.blade.php
@@ -1,5 +1,5 @@
 @extends('crudbooster::admin_template')
-@section('content')	 
+@section('content')
 
     <link rel="stylesheet" href="" {{asset('css/coderay.css')}}>
     <script src='{{asset("vendor/crudbooster/assets/jquery-sortable-min.js")}}'></script>
@@ -21,7 +21,7 @@
 		border:1px solid #0B2F64;
 	}
 	.field_fecha div{
-		
+
 		padding-left:20px;
 	}
 	.field_nameA {
@@ -39,7 +39,7 @@
 		text-transform: uppercase;
 	}
 	.field_date, .field_hour{
-		
+
 		line-height: 1.2em;
 		text-align: center;
 		padding: 10px 5px 10px 5px;
@@ -68,60 +68,60 @@
 		text-align: rigth;
 	}
 	</style>
-   
-	
-   
-  
+
+
+
+
 	<div class='row'>
 	<div class='col-sm-12'>
 	<form method='get' action='' id='form-privilege'>
        <div class='form-group'>
-		
-		
-		
+
+
+
 		<label class='label-control'>Campeonato</label>
-                <select class='form-control' onChange="$('#form-privilege').submit()" name='id_app_campeonato'>                  
+                <select class='form-control' onChange="$('#form-privilege').submit()" name='id_app_campeonato'>
                     @foreach($app_campeonato as $c)
-                      <option value='{{$c->id}}' {{($id_app_campeonato == $c->id)?"selected":""}}>{{$c->desNombre}}</option>
+                      <option value='{{$c->id}}' {{($id_app_campeonato == $c->id)?"selected":""}}>{{$c->desnombre}}</option>
                     @endforeach
 				</select>
-		
+
 			<label class='label-control'>Fecha</label>
-			<select class='form-control' onChange="$('#form-privilege').submit()" name='id_app_encuentrofecha'>                  
+			<select class='form-control' onChange="$('#form-privilege').submit()" name='id_app_encuentrofecha'>
 				@foreach($app_encuentrofecha as $ef)
-				  <option value='{{$ef->numFecha}}' {{($id_app_encuentrofecha == $ef->numFecha)?"selected":""}}>{{$ef->numFecha}}</option>
+				  <option value='{{$ef->numfecha}}' {{($id_app_encuentrofecha == $ef->numfecha)?"selected":""}}>{{$ef->numfecha}}</option>
 				@endforeach
 			</select>
-				
-		<br />		
-		
-	
+
+		<br />
+
+
 				<table id='table_dashboard' class="table table-hover table-striped table-bordered">
                     <thead>
-                    <tr class="active">                      
-                      
-                      <th width='3%'>Posicion</th>   
-					  <th width='10%'>Equipo</th>  
-					  <th width='3%'>PJ</th>  
-					  <th width='3%'>PG</th>  
-					  <th width='3%'>PE</th>  
-					  <th width='3%'>PP</th>  
-					  <th width='3%'>W.O.</th>  
-					  <th width='3%'>PF</th>  
-					  <th width='3%'>PC</th>  
-					  <th width='3%'>DP</th>  
-					  <th width='3%'>PB4</th>  
-					  <th width='3%'>PB7</th>  
-					  <th width='3%'>PTS</th>  
+                    <tr class="active">
+
+                      <th width='3%'>Posicion</th>
+					  <th width='10%'>Equipo</th>
+					  <th width='3%'>PJ</th>
+					  <th width='3%'>PG</th>
+					  <th width='3%'>PE</th>
+					  <th width='3%'>PP</th>
+					  <th width='3%'>W.O.</th>
+					  <th width='3%'>PF</th>
+					  <th width='3%'>PC</th>
+					  <th width='3%'>DP</th>
+					  <th width='3%'>PB4</th>
+					  <th width='3%'>PB7</th>
+					  <th width='3%'>PTS</th>
                     </tr>
                     </thead>
                     <tbody>
-                     
 
-				@foreach($table as $row) 
-					
+
+				@foreach($table as $row)
+
 						<tr>
-							
+
 							<td>1</td>
 							<td>{{$row["nombre"]}}</td>
 							<td>{{$row["numPartidosJugados"]}}</td>
@@ -136,42 +136,42 @@
 							<td>{{$row["numBonus7"]}}</td>
 							<td>{{$row["numPuntajeFinal"]}}</td>
 						</tr>
-				
-					
+
+
 				@endforeach
-              
-                    </tbody>  
+
+                    </tbody>
 
 
                     <tfoot>
-                    <tr>                      
-                      
-                    <th width='3%'>Posicion</th>   
-					  <th width='10%'>Equipo</th>  
-					  <th width='3%'>PJ</th>  
-					  <th width='3%'>PG</th>  
-					  <th width='3%'>PE</th>  
-					  <th width='3%'>PP</th>  
-					  <th width='3%'>W.O.</th>  
-					  <th width='3%'>PF</th>  
-					  <th width='3%'>PC</th>  
-					  <th width='3%'>DP</th>  
-					  <th width='3%'>PB4</th>  
-					  <th width='3%'>PB7</th>  
-					  <th width='3%'>PTS</th>                     
-                    </tr>
-                    </tfoot>               
-                  </table>    
-				  
+                    <tr>
 
-	
-		
-		
+                    <th width='3%'>Posicion</th>
+					  <th width='10%'>Equipo</th>
+					  <th width='3%'>PJ</th>
+					  <th width='3%'>PG</th>
+					  <th width='3%'>PE</th>
+					  <th width='3%'>PP</th>
+					  <th width='3%'>W.O.</th>
+					  <th width='3%'>PF</th>
+					  <th width='3%'>PC</th>
+					  <th width='3%'>DP</th>
+					  <th width='3%'>PB4</th>
+					  <th width='3%'>PB7</th>
+					  <th width='3%'>PTS</th>
+                    </tr>
+                    </tfoot>
+                  </table>
+
+
+
+
+
 	</div>
 	</form>
 	</div>
-         
-    </div>   
+
+    </div>
 	<!--END AUTO MARGIN-->
 
 @endsection
