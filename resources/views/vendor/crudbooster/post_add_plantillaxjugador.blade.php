987db884cb7be6f8e907b3cdafe09efa3a232b88
Jonatan E. Aburto <jonatan.app@gmail.com>
2018-03-18 09:29:57 -0500

    plantilla
    plantilla

--- resources/views/vendor/crudbooster/post_add_plantillaxjugador.blade.php
+++ resources/views/vendor/crudbooster/post_add_plantillaxjugador.blade.php
@@ -1,5 +1,5 @@
 @extends('crudbooster::admin_template')
-@section('content')	 
+@section('content')
 
     <link rel="stylesheet" href="" {{asset('css/coderay.css')}}>
          <style type="text/css">
@@ -10,7 +10,7 @@
             .dragged {
               position: absolute;
               opacity: 0.7;
-              z-index: 2000;              
+              z-index: 2000;
             }
 
             .draggable-menu {
@@ -21,7 +21,7 @@
             .draggable-menu li ul {
               margin-top:6px;
             }
-  
+
             .draggable-menu li div {
               padding:5px;
               border:1px solid #cccccc;
@@ -30,15 +30,15 @@
             }
 
             .draggable-menu li .is-dashboard {
-              background: #fff6e0;              
-            }  
+              background: #fff6e0;
+            }
 
             .draggable-menu li .icon-is-dashboard {
               color: #ffb600;
             }
 
             .draggable-menu li {
-              list-style-type:none;                            
+              list-style-type:none;
               margin-bottom:4px;
               min-height: 35px;
             }
@@ -56,12 +56,12 @@
          </style>
          <script src='{{asset("vendor/crudbooster/assets/jquery-sortable-min.js")}}'></script>
   <script type="text/javascript">
-  
+
            $(function  () {
             var id_app_plantilla = {{$id_app_plantilla}};
             var sortactive = $(".draggable-menu").sortable({
-              group: '.draggable-menu',  
-              delay:200,               
+              group: '.draggable-menu',
+              delay:200,
               isValidTarget: function ($item, container) {
                   var depth = 1, // Start with a depth of one (the element itself)
                       maxDepth = 2,
@@ -77,13 +77,13 @@
                   }
 
                   return depth <= maxDepth;
-              },                       
-              onDrop: function ($item, container, _super) {                                                    
+              },
+              onDrop: function ($item, container, _super) {
 
                 if($item.parents('ul').hasClass('draggable-menu-active')) {
                   var isActive = 1;
                   var data = $('.draggable-menu-active').sortable("serialize").get();
-                  var jsonString = JSON.stringify(data, null, ' '); 
+                  var jsonString = JSON.stringify(data, null, ' ');
                 }else{
                   var isActive = 0;
                   var data = $('.draggable-menu-inactive').sortable("serialize").get();
@@ -94,19 +94,19 @@
                 $.post("{{route('AdminAppPlantillaxjugadorControllerPostSaveTemplate')}}",{plantilla:jsonString,plantillaid:id_app_plantilla},function(resp) {
                   $('#menu-saved-info').fadeIn('fast').delay(1000).fadeOut('fast');
                 });
-				
-				
+
+
                 _super($item, container);
               }
-            });    
+            });
 
 
 
           });
          </script>
-		 
-   
-   
+
+
+
    <script type="text/javascript">
    /*
    $(function  () {
@@ -125,7 +125,7 @@
 				  $clonedItem.detach();
 				  _super($item, container);
 				});
-				
+
 				var data = $('ol.simple_with_animation').sortable("serialize").get();
                   var jsonString = JSON.stringify(data, null, ' ');
 				 // alert( jsonString);
@@ -153,14 +153,14 @@
 	});
 	*/
   </script>
-  
+
 	<div class='row'>
             <div class='col-sm-12'>
 
               <form method='get' action='' id='form-privilege'>
                 <div class='form-group'>
 					<label class='label-control'>Plantillas Disponibles</label>
-					<select class='form-control' onChange="$('#form-privilege').submit()" name='id_app_plantilla'>                  
+					<select class='form-control' onChange="$('#form-privilege').submit()" name='id_app_plantilla'>
 						@foreach($plantilla as $p)
 						  <option value='{{$p->id}}' {{($id_app_plantilla == $p->id)?"selected":""}}>{{$p->desPlantilla}}</option>
 						@endforeach
@@ -169,12 +169,12 @@
                 </div>
               </form>
             </div>
-    </div>        
+    </div>
 	<div class='row'>
 		<div class="col-sm-5">
 			<div class="panel panel-success">
 				<div class="panel-heading">
-                    <strong>Listado de Jugadores Disponibles</strong> 
+                    <strong>Listado de Jugadores Disponibles</strong>
 					<span id='menu-saved-info' style="display:none" class='pull-right text-success'>
 						<i class='fa fa-check'></i> Actualizando !
 					</span>
@@ -183,10 +183,10 @@
 					<ol class="simple_with_animation vertical">
 					<ul class='draggable-menu draggable-menu-active'>
 						@foreach($jugadores as $c)
-						<li data-id='{{$c->id}}' data-name='{{$c->desNombre}}'>
-							<div class='' title="{{$c->desApellidoPaterno}} {{$c->desNombre}} ">
+						<li data-id='{{$c->id}}' data-name='{{$c->desnombre}}'>
+							<div class='' title="{{$c->desapellidopaterno}} {{$c->desnombre}} ">
 								<i class='fa fa-user'></i>
-								{{$c->desApellidoPaterno}} {{$c->desNombre}} 
+								{{$c->desapellidopaterno}} {{$c->desnombre}}
 							</div>
 						</li>
 						@endforeach
@@ -194,56 +194,56 @@
 					</ol>
 				</div>
 			</div>
-		</div>	
+		</div>
 
 		<div class="col-sm-7">
 			<div class="panel panel-success">
 				<div class="panel-heading">
-                    <strong>Jugadores seleccionados</strong> 
+                    <strong>Jugadores seleccionados</strong>
 					<span id='menu-saved-info' style="display:none" class='pull-right text-success'>
 						<i class='fa fa-check'></i> Actualizando !
 					</span>
                 </div>
-			
+
 			<div class="panel-body clearfix">
 				<ol class="simple_with_animation vertical">
 				<ul class='draggable-menu draggable-menu-active'>
-					
+
 					@foreach($plantillaxjugador as $pj)
 					<li data-id='{{$pj->id}}' data-name='{{$pj->desNombre}}'>
 						<div class='' title="{{$pj->desApellidoPaterno}} {{$pj->desNombre}}">
-							<i class='fa fa-user'></i> {{$pj->desApellidoPaterno}} {{$pj->desNombre}}   
-							
+							<i class='fa fa-user'></i> {{$pj->desApellidoPaterno}} {{$pj->desNombre}}
+
 						</div>
 					</li>
 					@endforeach
-					
+
 				</ul>
 				</ol>
 			</div>
 			</div>
-		</div>		
+		</div>
 	</div>
 	<!--
 	<div class="panel-body clearfix">
 		<ol class="simple_with_animation vertical">
         <ul class='draggable-menu draggable-menu-active'>
-			
+
 			<li>
 				<div class='{{$menu->is_dashboard?"is-dashboard":""}}' title="{{$menu->is_dashboard?'This is setted as Dashboard':''}}">
-                    <i class='{{($menu->is_dashboard)?"icon-is-dashboard fa fa-dashboard":$menu->icon}}'></i> 
-						
+                    <i class='{{($menu->is_dashboard)?"icon-is-dashboard fa fa-dashboard":$menu->icon}}'></i>
+
 					</div>
 			</li>
-			
+
 		</ul>
 		</ol>
-	</div>	
-		
+	</div>
+
 	<div class='row'>
 		<div class="span4">
 			<ol class="simple_with_animation vertical">
-                
+
 			</ol>
 		</div>
 		<div class="span4">
